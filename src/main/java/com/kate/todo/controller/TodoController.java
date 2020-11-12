b4288fe1544cd32930d779012d27b5298f7964ed
Oleg Zaidullin <cornknight@gmail.com>
2020-11-12 12:40:43 +0600

    update and delete are working

--- src/main/java/com/kate/todo/controller/TodoController.java
+++ src/main/java/com/kate/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -23,6 +24,17 @@ public class TodoController {
 		this.repo = repo;
 	}
 
+	@GetMapping("/{id}")
+	public TodoItemDTO get(@PathVariable Long id) throws Exception {
+		Optional<TodoItem> itemOpt = repo.findById(id);
+		if(itemOpt.isPresent()) {
+			TodoItem item = itemOpt.get();
+			return item.toDTO();
+		} else {
+			throw new Exception("No such object");
+		}
+	}
+
 	@GetMapping("/list")
 	public List<TodoItemDTO> getTodoList() {
 		return repo.findAll().stream()
@@ -34,5 +46,22 @@ public class TodoController {
 	public TodoItemDTO add(@RequestBody TodoItemDTO dto) {
 		TodoItem item = dto.toEntity();
 		return repo.save(item).toDTO();
+	}
+
+	@DeleteMapping(value = "/delete/{id}")
+	public void delete(@PathVariable Long id) {
+		repo.deleteById(id);
+	}
+
+	@PutMapping(value = "/update")
+	public TodoItemDTO update(@RequestBody TodoItemDTO dto) throws Exception {
+		Optional<TodoItem> itemOpt = repo.findById(dto.getId());
+		if(itemOpt.isPresent()) {
+			TodoItem item = itemOpt.get();
+			item.setTitle(dto.getTitle());
+			return repo.save(item).toDTO();
+		} else {
+			throw new Exception("No such object");
+		}
 	}
 }
